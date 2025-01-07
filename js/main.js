26c34187f5b66e446bb0e94c0fc5a50a064ddaa6
Misha Apurin <hostmapua@gmail.com>
2025-01-07 08:47:21 +0200

    update task 3_1 transfer code index.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 "use strict";
 
-document.getElementById("message").innerText = "Didko ";
+// document.getElementById("message").innerText = "Didko ";
 
-console.log("a".codePointAt(0));
-console.log(String.fromCodePoint(1250));
+// console.log("a".codePointAt(0));
+// console.log(String.fromCodePoint(1250));
+// console.log("Їхав грека через ріку" > "іхав грека через ріку");
+// console.log("ї".codePointAt(0));
+// console.log("і".codePointAt(0));
+// console.log(
+//   "Їхав грека через ріку".localeCompare("іхав грека через ріку", "ua")
+// );
+// const fruit = ["zarra", "banana", "apple"];
+
+// console.log(fruit.pull(2));
+const scores = [10, 8, 7, 12, 2, 65];
+const goodScores = scores.filter((score) => score > 8);
+console.log(goodScores);
