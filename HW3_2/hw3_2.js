bc25697b5891fe6ecb2c3a0cb33446a34dd4de52
Misha Apurin <hostmapua@gmail.com>
2025-01-07 07:58:19 +0200

    add task 3_2

---
+++ HW3_2/hw3_2.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const rows = [
+  prompt("Введіть рядок №1"),
+  prompt("Введіть рядок №2"),
+  prompt("Введіть рядок №3"),
+];
+
+console.log(`Всі рядки разом: 
+    1. ${rows[0]}
+    2. ${rows[1]}
+    3. ${rows[2]}`);
