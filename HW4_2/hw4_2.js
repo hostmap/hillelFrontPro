30ef5c1ba4c533feb6f58d2a9dc5cf8df6acad90
Misha Apurin <hostmapua@gmail.com>
2025-01-09 23:09:08 +0200

    task 4_2

---
+++ HW4_2/hw4_2.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const num = prompt("Введіть тризначне число");
+function checkNum(num) {
+  const hundrs = Math.floor(num / 100);
+  const tens = Math.floor((num % 100) / 10);
+  const singles = num % 10;
+
+  switch (true) {
+    case hundrs === tens && tens === singles:
+      console.log("Всі числа однакові");
+      break;
+    case hundrs === tens:
+      console.log("Сотні та десятки однакові");
+      break;
+    case hundrs === singles:
+      console.log("Сотні та одиниці однакові");
+      break;
+    case tens === singles:
+      console.log("Десятки та одиниці однакові");
+      break;
+    default:
+      console.log("Всі числа різні");
+  }
+}
+
+console.log(checkNum(num));
