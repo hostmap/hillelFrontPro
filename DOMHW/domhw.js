f4f9954d3f19920f98d2f3f208359d339a2545d7
Misha <hostmapua@gmail.com>
2025-02-18 18:23:01 +0200

    add tasks 7_3 update

---
+++ DOMHW/domhw.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// console.dir(document);
+// console.log(document.getElementById("teg-id"));
+// console.log(document.getElementsByClassName("teg-class"));
+// console.log(document.getElementsByTagName("p"));
+// console.log(document.querySelector('.teg-class:last-child'));
+// console.log(document.querySelectorAll('p:nth-child(2n)'));
+
+// const htmlCollection = document.getElementsByTagName('p');
+// const nodeListColection = document.querySelectorAll('p');
+
+// const pTags = document.querySelectorAll('p');
+// for (const tag of pTags) {
+//     console.log(tag);
+//     console.log(pTags)
+// }
+
+// document.body;
+// document.forms;
+// document.title;
+// document.head;
+// document.images;
+// document.links;
+// document.documentElement;
+
+const divElem = document.querySelector('div');
+// console.log(divElem);
+// const childElem = divElem.children;
+// const childNoda = divElem.childNodes;
+// console.log(divElem.innerText);
+// console.log(divElem.innerHTML);
+// console.log(divElem.textContent);
+// divElem.innerText = "Все заново";
+// divElem.textContent = "Все заново";
+// divElem.innerHTML = "<h1>Все заново</h1>";
--- HW7_3/hw7_3.js
+++ HW7_3/hw7_3.js
@@ -2,11 +2,15 @@
 
 function moreHundred() {
   let num;
+  let iter = 0;
+
   for (let i = 0; i < 10; i++) {
+    iter++;
     num = prompt("Введіть число більше 100:");
 
     if (num === null) {
       console.log("Користувач скасував введення.");
+      console.log("Пройдено ітерацій: " + iter);
       return;
     }
 
@@ -14,8 +18,16 @@ function moreHundred() {
 
     if (isNaN(num)) {
       console.log("Ви ввели не число.");
+      console.log("Пройдено ітерацій: " + iter);
+      if (confirm("Продовжити введення?")) {
+        continue;
+      } else {
+        console.log("Ви завершили введення чисел");
+        return;
+      }
     } else if (num > 100) {
       console.log("Останнє введене число: " + num);
+      console.log("Пройдено ітерацій: " + iter);
       return;
     } else {
       console.log("Число менше 100. Спробуйте ще раз.");
@@ -23,6 +35,7 @@ function moreHundred() {
   }
 
   console.log("Ви перевищили ліміт спроб.");
+  console.log("Пройдено ітерацій: " + iter);
 }
 
 moreHundred();
